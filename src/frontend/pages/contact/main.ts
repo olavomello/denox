931c7de0b1ee2c153e27cb228cf20fb84e5d03ca
Olavo Mello <olavo@agenciadix.com.br>
2026-07-05 15:50:14 -0400

    frontend: add form and form style

--- src/frontend/pages/contact/main.ts
+++ src/frontend/pages/contact/main.ts
@@ -10,7 +10,7 @@ export const config = {
   meta: {
     title: "Contact",
     description:
-      "DenoX contact page made easy just creating a new <b>pages/folder</b> and managing the informations on <b>main.ts</b> file.",
+      "Creating contact page is easy: simply create a new <b>pages</b> folder or file and manage the information in the <b>main.ts</b> file.",
   },
 } as const;
 
@@ -24,5 +24,16 @@ export default function aboutPage(_c: Context): string {
   return `
     <h1>${config.meta.title}</h1>
     <p>${config.meta.description} </p>
+    <br>
+    <!-- Form example -->
+    <form action="/contact" method="post">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" required>
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required>
+      <label for="message">Message:</label>
+      <textarea id="message" name="message" required></textarea>
+      <button type="submit">Submit</button>
+    </form>
   `;
 }
